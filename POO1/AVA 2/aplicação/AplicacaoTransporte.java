347dca4e5d21b12174582202e37e83d0b3a48978
diegoguiomar <67815905+diegoguiomar@users.noreply.github.com>
2020-09-20 22:30:24 -0300

    Update AplicacaoTransporte.java

--- POO1/AVA 2/aplicação/AplicacaoTransporte.java
+++ POO1/AVA 2/aplicação/AplicacaoTransporte.java
@@ -7,26 +7,44 @@ public class AplicacaoTransporte {
 		// TODO Auto-generated method stub
 		Aviao aviao[] = new Aviao[10];
 		Navio navio[] = new Navio[10];
-		
+
 		int opcao;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Qual transporte você deseja cadastrar?");
 		System.out.println("Digite 1 para avião ou 2 para navio");
 		opcao = Integer.parseInt(sc.nextLine());
-		sc.close();
-		
-		if(opcao == 1) {
-			System.out.println("Cadastro do avião!");
-			for(int i = 0; i < aviao.length; i++) {
-				aviao[i] = new Aviao();
-				aviao[i].entradaDados();
+
+		try {
+			if(opcao == 1) {
+				for(int i = 0; i < aviao.length; i++) {
+					System.out.println("Cadastro do avião " + i + "!");
+					aviao[i] = new Aviao();
+					aviao[i].entradaDados();
+					aviao[i].reajustarPreco();
+					System.out.println(""); 
+				}
+				for(int i = 0; i < aviao.length; i++) {
+					System.out.println("Avião " + i + ":");
+					aviao[i].imprimir();
+					System.out.println(""); 
+				}
+			} 
+			if(opcao == 2) {
+				for(int i = 0; i < navio.length; i++) {
+					navio[i] = new Navio();
+					navio[i].entradaDados();
+					System.out.println(""); 
+				}
+				for(int i = 0; i < navio.length; i++) {
+					System.out.println("Navio " + i + ":");
+					navio[i].imprimir();
+					navio[i].passageirosPorTripulantes();
+					System.out.println(""); 
+				}
 			}
-		} 
-		if(opcao == 2) {
-			for(int j = 0; j < navio.length; j++) {
-				navio[j] = new Navio();
-				navio[j].entradaDados();
-			}
+		}catch(Exception ex) {
+			System.out.println("Erro encontrado: ");
+			System.out.println(ex);
 		}
 	}
 }
